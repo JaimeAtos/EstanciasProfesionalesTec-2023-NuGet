f3462a251bd2be3613c2eebc0c2c89bce6ca49e9
Avir <avirgariel@gmail.com>
2023-11-01 15:00:40 -0600

    BrokerDtos and IAwsMessage added

---
+++ EstanciasProfesionalesTec.Core/Abstractions/IAwsMessage.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstanciasProfesionalesTec.Core.Abstractions;
+
+public interface IAwsMessage : IRequest
+{
+
+}
+
+//Sns
+public interface ISnsMessage : IAwsMessage
+{
+
+}
+
+//Sqs
+public interface ISqsMessage : IAwsMessage
+{
+
+}
---
+++ EstanciasProfesionalesTec.Core/BrokerDto/ClientMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstanciasProfesionalesTec.Core.BrokerDto;
+
+public record ClientMessage
+{
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+    public string FullName { get; set; } = default!;
+}
